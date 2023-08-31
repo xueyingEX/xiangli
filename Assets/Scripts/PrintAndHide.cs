2f43ae81a81a557769a5016262552d969159c269
xueyingEX <2977316@gmail.com>
2023-08-31 16:00:06 +1000

    90 percent complete

---
+++ Assets/Scripts/PrintAndHide.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    public Renderer rend;
+    private int i;
+    private int randomInt;
+    // Start is called before the first frame update
+    void Start()
+    {
+         i = 3;
+        randomInt = Random.Range(150, 251);  // Generates a random integer between 150 and 250 inclusive
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         i++;
+        Debug.Log(gameObject.name + ":" + i);
+
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);  // Deactivates the GameObject
+        }
+
+        if (gameObject.tag == "Blue" && i == randomInt)
+        {
+            rend.enabled = false;  // Disables the Renderer component
+        }
+        
+    }
+}
